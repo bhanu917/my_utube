0becfb8e008a794df7f5ba3834053fe8697c6e21
bhanu917 <bhanuprakashu1@gmail.com>
2026-06-30 10:40:13 +0530

    Add vercel.json and update vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my_utube/',
+  // Use different base depending on environment
+  base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/my_utube/' : '/',
 })
